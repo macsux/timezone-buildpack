f7fd95df67bc6fbf1385e92ed9e6d48abf937e6d
Andrew Stakhov <andrew.stakhov@gmail.com>
2020-11-02 13:45:13 -0500

    Initial

---
+++ src/TimezoneBuildpackModule/TimezoneBuildpackHttpModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using HarmonyLib;
+
+namespace TimezoneBuildpackModule
+{
+    public class TimezoneBuildpackHttpModule: IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            var harmony = new Harmony("TimezoneBuildpack");
+            harmony.PatchAll();
+        }
+       
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ src/TimezoneBuildpackModule/TimezoneBuildpackPatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HarmonyLib;
+
+namespace TimezoneBuildpackModule
+{
+    [HarmonyPatch(typeof(DateTime), nameof(DateTime.Now), new Type[0])]
+    public class MyPatch
+    {
+        static bool Prefix(ref DateTime __result)
+        {
+            TimeZoneInfo timezoneInfo;
+            try
+            {
+                timezoneInfo = TimeZoneInfo.FindSystemTimeZoneById(Environment.GetEnvironmentVariable("TIMEZONE") ?? "UTC");
+            }
+            catch (Exception)
+            {
+                timezoneInfo = TimeZoneInfo.Utc;
+            }
+            __result = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timezoneInfo);
+            return false;
+        }
+    }
+}
